00ddf326e9d82b0c75a5b3d9fbc2d3e127e58a97
mutasem <motasemimad10@gmail.com>
2020-11-03 15:13:56 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,77 +1,121 @@
-var memory = 0;
-var memorycalc;
-var c = false;
-var flag = false;
-var screen = function(p) {
-  if (p == '.' && flag == true) {
-    return;
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement){
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
   }
-  if (c == true) {
-    $('#screen').val('');
-    c = false;
+  clear(){
+    this.currentOperand = ''
+    this.previousOperand =''
+    this.operation = undefined
   }
-  var r = $('#screen').val('') + p;
-  if (p == '.') {
-    flag = true;
-  } else {
-    r = r * 1;
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1)
   }
-  $('#screen').val('');
-};
-var calculate = function(p) {
-  if (memory) {
-    result();
+  appendNumber(number) {
+    if(number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
   }
-  flag = false;
-  c = true;
-  memory = $('#screen').val('');
-  memorycalc = p;
-};
-$('#clear').click(function() {
-  memory = 0;
-  $('#screen').val('');
-});
-$('#sign').click(function() {
-  $('#screen').val($('#screen').val('') * -1);
-});
-$('#backspace').click(function() {
-  var len = $('#screen').val().length;
-  $('#screen').val(
-    $('#screen')
-      .val()
-      .substring(0, len - 1)
-  );
-  if ($('#screen').val().length == 0) $('#screen').val(0);
-});
-var result = function() {
-  if (memory == 0) return;
-  c = true;
-  var r;
-  switch (memorycalc) {
-    case '+':
-      r = memory + $('#screen').val();
-      break;
-    case '-':
-      r = memory - $('#screen').val();
-      break;
-    case '*':
-      r = memory * $('#screen').val();
-      break;
-    case '/':
-      r = memory / $('#screen').val();
-      break;
+  chooseOperation(operation){
+    if(this.currentOperand === '') return
+    if(this.previousOperand !== '') {
+      this.compute()
+    }
+    this.operation = operation
+    this.previousOperand = this.currentOperand
+    this.currentOperand = ''
   }
-  screen(r);
-  c = true;
-  flag = false;
-  memory = 0;
-};
-$('#degits').click(function(e) {
-  screen(e.target.value);
-});
-$('#divide , #multiply , #minus , #plus').click(function(e) {
-  calculate(e.target.value);
-});
-$('#equal').click(function() {
-  result();
-});
+  compute(){
+    let computation 
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if(isNaN(prev) || isNaN(current)) return
+    switch (this.operation) {
+      case '+':
+      computation = prev + current
+      break
+      case '-':
+      computation = prev - current
+      break
+      case '*':
+      computation = prev * current
+      break
+      case '÷':
+      computation = prev / current
+      break
+      default: return
+    }
+    this.currentOperand = computation
+    this.operation = undefined
+    this.previousOperand = ''
+  }
+
+  getDisplayNumber (number) {
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+    if(isNaN(integerDigits)){
+      integerDisplay = ''
+    }else{
+      integerDisplay = integerDigits.toLocaleString('en',{
+        maximumFractionDigits: 0
+      })
+    }
+    if(decimalDigits != null){
+      return `${integerDisplay}.${decimalDigits}`
+    }else{
+      return integerDisplay
+    }
+  }
+
+  updateDisplay(){
+    this.currentOperandTextElement.innerText = 
+    this.getDisplayNumber(this.currentOperand)
+    if(this.operation != null){
+      this.previousOperandTextElement.innerText =
+      `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    }else{
+      this.previousOperandTextElement.innerText = ''
+    }
+  }
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const AllClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })})
+
+  operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.chooseOperation(button.innerText)
+      calculator.updateDisplay()
+    })})
+  
+equalsButton.addEventListener('click', button => {
+  calculator.compute()
+  calculator.updateDisplay()
+})
+
+AllClearButton.addEventListener('click', button => {
+  calculator.clear()
+  calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+  calculator.delete()
+  calculator.updateDisplay()
+})
